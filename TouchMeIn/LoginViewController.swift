f64a1fe08b70ac49f1924bfa67b636295a302afb
Lane Faison <lane.faison@icloud.com>
2017-11-24 13:52:04 -0700

    Password authentication working

--- TouchMeIn/LoginViewController.swift
+++ TouchMeIn/LoginViewController.swift
@@ -1,43 +1,126 @@
 /*
-* Copyright (c) 2017 Razeware LLC
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*/
+ * Copyright (c) 2017 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 import UIKit
 import CoreData
 
-class LoginViewController: UIViewController {
-  
-  var managedObjectContext: NSManagedObjectContext?
-  
-  @IBOutlet weak var usernameTextField: UITextField!
-  @IBOutlet weak var passwordTextField: UITextField!
-  @IBOutlet weak var createInfoLabel: UILabel!  
+struct KeychainConfiguration {
+    static let serviceName = "TouchMeIn"
+    static let accessGroup: String? = nil
+}
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
-  
-  // MARK: - Action for checking username/password
-  @IBAction func loginAction(_ sender: AnyObject) {
-    self.performSegue(withIdentifier: "dismissLogin", sender: self)
-  }
-  
+class LoginViewController: UIViewController {
+    
+    var managedObjectContext: NSManagedObjectContext?
+    var passwordItems: [KeychainPasswordItem] = []
+    let createLoginButtonTag = 0
+    let loginButtonTag = 1
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var createInfoLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let hasLogin = UserDefaults.standard.bool(forKey: "hasLoginKey")
+        
+        if hasLogin {
+            loginButton.setTitle("Login", for: .normal)
+            loginButton.tag = loginButtonTag
+            createInfoLabel.isHidden = true
+        } else {
+            loginButton.setTitle("Create", for: .normal)
+            loginButton.tag = createLoginButtonTag
+            createInfoLabel.isHidden = false
+        }
+        
+        if let storedUsername = UserDefaults.standard.value(forKey: "username") as? String {
+            usernameTextField.text = storedUsername
+        }
+    }
+    
+    // MARK: - Action for checking username/password
+    @IBAction func loginAction(_ sender: AnyObject) {
+        guard let newAccountName = usernameTextField.text,
+              let newPassword = passwordTextField.text,
+            !newAccountName.isEmpty && !newPassword.isEmpty else {
+                
+                let alertView = UIAlertController(title: "Login Problem", message: "Wrong username or password", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "Foiled Again!", style: .default, handler: nil)
+                alertView.addAction(okAction)
+                present(alertView, animated: true, completion: nil)
+                return
+        }
+        
+        usernameTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        
+        if sender.tag == createLoginButtonTag {
+            let hasLoginKey = UserDefaults.standard.bool(forKey: "hasLoginKey")
+            if !hasLoginKey {
+                UserDefaults.standard.setValue(usernameTextField.text, forKey: "username")
+            }
+            
+            do {
+                let passwordItem = KeychainPasswordItem(service: KeychainConfiguration.serviceName, account: newAccountName, accessGroup: KeychainConfiguration.accessGroup)
+                
+                try passwordItem.savePassword(newPassword)
+            } catch {
+                fatalError("Error updating keychain - \(error)")
+            }
+            
+            UserDefaults.standard.set(true, forKey: "hasLoginKey")
+            loginButton.tag = loginButtonTag
+            
+            performSegue(withIdentifier: "dismissLogin", sender: self)
+        } else if sender.tag == loginButtonTag {
+            if checkLogin(username: usernameTextField.text!, password: passwordTextField.text!) {
+                performSegue(withIdentifier: "dismissLogin", sender: self)
+            } else {
+                let alertView = UIAlertController(title: "Login Problem", message: "Wrong username or password", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "Foiled Again!", style: .default)
+                alertView.addAction(okAction)
+                present(alertView, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    func checkLogin(username: String, password: String) -> Bool {
+        guard username == UserDefaults.standard.value(forKey: "username") as? String else {
+            return false
+        }
+        
+        do {
+            let passwordItem = KeychainPasswordItem(service: KeychainConfiguration.serviceName, account: username, accessGroup: KeychainConfiguration.accessGroup)
+            let keychainPassword = try passwordItem.readPassword()
+            return password == keychainPassword
+        } catch {
+            fatalError("Error reading password from keychain - \(error)")
+        }
+        
+        return false
+    }
+    
 }
